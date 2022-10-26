405101bb30d615d53909d3c8cb175ae97fa5973e
astsu <you@example.com>
2022-10-26 13:38:47 +0300

    Add email form popup.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
-(() => {
+const registerPopup = (selector: string) => {
+  const popup = document.querySelector(`${selector}`);
+  const overlay = document.querySelector(`${selector} .popup__overlay`);
+  const closeButton = document.querySelector(
+    `${selector} .popup__close-button`
+  );
+  const openButtons = document.querySelectorAll(`${selector}-open-button`);
+
+  const togglePopup = () => {
+    popup?.classList.toggle('popup_active');
+  };
+
+  [overlay, closeButton, ...Array.from(openButtons)].forEach(toggler =>
+    toggler?.addEventListener('click', togglePopup)
+  );
+};
+
+const registerMenu = () => {
   const header = document.querySelector('.header');
   const menuButton = document.querySelector('.menu-button');
   const body = document.querySelector('body');
 
-  const onMenuButtonClick = (e: TouchEvent) => {
+  const onMenuButtonClick = (e: Event) => {
     e.preventDefault();
-    header && header.classList.toggle('header_menu-active');
-    body && body.classList.toggle('no-scroll');
-    menuButton && menuButton.classList.toggle('menu-button_active');
+    header?.classList.toggle('header_menu-active');
+    body?.classList.toggle('no-scroll');
+    menuButton?.classList.toggle('menu-button_active');
   };
 
-  menuButton && menuButton.addEventListener('click', onMenuButtonClick);
+  menuButton?.addEventListener('click', onMenuButtonClick);
+};
+
+(() => {
+  registerMenu();
+  registerPopup('.form-popup');
 })();
